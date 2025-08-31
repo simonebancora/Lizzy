0c2f3e93b08c0e77872e0d59ad398f6eba5d7e0e
simonebancora <simone.bancora@gmail.com>
2025-08-31 01:43:21 +0100

    updated some tutorials

--- examples/scripts/channel_infusion.py
+++ examples/scripts/channel_infusion.py
@@ -2,7 +2,7 @@ import lizzy as liz
 
 model = liz.LizzyModel()
 model.read_mesh_file("../meshes/Rect1M_R2.msh")
-model.assign_simulation_parameters(mu=0.1, wo_delta_time=100, fill_tolerance=0.01, generate_fill_image = False, display_fill = False)
+model.assign_simulation_parameters(mu=0.1, wo_delta_time=100, fill_tolerance=0.01)
 model.create_material(1E-10, 1E-10, 1E-10, 0.5, 1.0, "domain_material")
 model.assign_material("domain_material", 'domain')
 model.create_sensor(0.2, 0.25, 0)
