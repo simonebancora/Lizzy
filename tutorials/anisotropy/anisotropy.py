a169fee541d85c2929a55e018e4ed8cb8d8a0ab9
simonebancora <simone.bancora@gmail.com>
2026-04-14 21:06:25 +0100

    updated scripts in tutorials

--- tutorials/anisotropy/anisotropy.py
+++ tutorials/anisotropy/anisotropy.py
@@ -1,26 +1,33 @@
 import lizzy
 
+# Set up logging level
 import logging
 logging.basicConfig(level=logging.INFO)
 
+# Set up the model: save results every 60 seconds of process time
 model = lizzy.LizzyModel()
 model.read_mesh_file("Anisotropy.msh")
 model.set_simulation_parameters(output_interval=60, progress_bar=True)
 
+# Create a resin and assign it
 model.create_resin("resin", 0.1)
 model.assign_resin("resin")
 
+# Create an anisotropic material, an orientation rosette and assign both to the domain
 model.create_rosette("rosette", (1,1,0))
 model.create_material("aniso_material", (1E-10, 1E-11, 1E-10), 0.5, 0.005)
 model.assign_material("aniso_material", 'domain', "rosette")
 
+# Boundary conditions
 model.create_pressure_inlet("inner_inlet", 1E+05)
 model.assign_inlet("inner_inlet", "inner_rim")
 
 model.create_vent("outer_vent", 0.0)
 model.assign_vent("outer_vent", "outer_rim")
 
+# Solve
 model.initialise_solver()
 model.solve()
 
+# Save results
 model.save_results()
--- tutorials/racetracking/racetracking.py
+++ tutorials/racetracking/racetracking.py
@@ -1,31 +1,39 @@
 import lizzy
 
+# Set up logging level
 import logging                                                                                                          
 logging.basicConfig(level=logging.INFO)
 
+# calculations for the racetrack permeability (2 mm wide, lubrication theory)
 racetrack_gap = 0.002
 racetrack_perm = racetrack_gap**2 / 12
 
+# Set up the model: save results every 5 seconds of process time
 model = lizzy.LizzyModel()
 model.read_mesh_file("ChannelRacetrack.msh")
 model.set_simulation_parameters(output_interval=5, progress_bar=True)
 
+# Create a resin and assign it
 model.create_resin("resin_01", 0.1)
 model.assign_resin("resin_01")
 
+# Create 2 different materials and assign them: one for the porous domain, one for the racetrack
 model.create_material("domain_material", (1E-10, 1E-10, 1E-10), 0.5, 0.005)
 model.assign_material("domain_material", 'domain')
 
 model.create_material("racetrack_material", (racetrack_perm, racetrack_perm, racetrack_perm), 1.0, 0.005)
 model.assign_material("racetrack_material", 'racetrack')
 
+# Boundary conditions
 model.create_pressure_inlet("inlet_left", 1E+05)
 model.assign_inlet("inlet_left", "left_edge")
 
 model.create_vent("vent_right", 0.0)
 model.assign_vent("vent_right", "right_edge")
 
+# Solve
 model.initialise_solver()
 model.solve()
 
+# Save results
 model.save_results()
