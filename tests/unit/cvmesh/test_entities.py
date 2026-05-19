36de6181a932800987f6e8fa1055e34e32302aaf
simonebancora <simone.bancora@gmail.com>
2026-05-19 12:21:54 +0200

    sped up fill test

--- tests/unit/cvmesh/test_entities.py
+++ tests/unit/cvmesh/test_entities.py
@@ -21,7 +21,7 @@ def test_node_constructor():
     np.testing.assert_array_equal(coords, node.coords)
 
 
-def test_triangle_constructor():
+def test_triangle_constructor(xy_tri):
     # oblique triangle
     n0 = Node(1, 0, 0, 0)
     n1 = Node(0, 1, 0, 1)
@@ -30,20 +30,14 @@ def test_triangle_constructor():
     l12 = Line(n1, n2, 11)
     l20 = Line(n2, n0, 12)
     tri = Triangle(n0, n1, n2, l01, l12, l20, 10)
-    assert tri.idx == 10
-    assert tri.nodes == (n0, n1, n2)
-    assert tri.node_ids == (0, 1, 2)
-    assert tri.line_ids == (10, 11, 12)
+
     b = np.sqrt(2)
     h = np.sqrt((0.5 * np.sqrt(2)) ** 2 + 1)
     np.testing.assert_equal(tri.A, b * h / 2)
     mag = np.linalg.norm(np.array([1, 1, 1]))
-    np.testing.assert_almost_equal(
-        np.abs(np.dot(tri.n, np.array([1 / mag, 1 / mag, 1 / mag]))), 1.0
-    )
-
-def test_centroid(xy_tri):
-    np.testing.assert_allclose(xy_tri.centroid, [1/3, 1/3, 0.0])
+    np.testing.assert_almost_equal(np.abs(np.dot(tri.n, np.array([1 / mag, 1 / mag, 1 / mag]))), 1.0)
+    # test centroid on the xy triangle
+    np.testing.assert_allclose(xy_tri.centroid, [1/3, 1/3, 0.0])    
 
 def test_shape_functions(xy_tri):
     # sum of shape function gradients must be zero
@@ -51,7 +45,6 @@ def test_shape_functions(xy_tri):
     # grad_N @ nodal values
     node_x = np.array([n.coords[0] for n in xy_tri.nodes])
     np.testing.assert_allclose(xy_tri.grad_N @ node_x, [1.0, 0.0, 0.0], atol=1e-12)
-
     node_y = np.array([n.coords[1] for n in xy_tri.nodes])
     np.testing.assert_allclose(xy_tri.grad_N @ node_y, [0.0, 1.0, 0.0], atol=1e-12)
 
